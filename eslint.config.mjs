5fefc58cb38cb80f4b8a6d432971e701660db6b1
JusGaga <gaetan3107@gmail.com>
2025-02-28 12:56:15 +0100

    init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest", // Active les dernières fonctionnalités JS
+      sourceType: "module",  // Si tu utilises import/export, sinon mets "script"
+      globals: {
+        ...globals.node // Active les globales de Node.js (process, require, etc.)
+      }
+    },
+    rules: {
+      "no-undef": "off" // Désactive l’erreur sur require et process
+    }
+  },
+  pluginJs.configs.recommended
 ];
